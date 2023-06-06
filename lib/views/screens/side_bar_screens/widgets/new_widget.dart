cac9058aaed332c5891c44786c44b56e0480bc54
amrkhx <amrkhah2@gmail.com>
2023-06-06 21:13:11 +0300

    orders

--- lib/views/screens/side_bar_screens/widgets/new_widget.dart
+++ lib/views/screens/side_bar_screens/widgets/new_widget.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
 
 class OrderWidget1 extends StatelessWidget {
   const OrderWidget1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-      stream: FirebaseFirestore.instance.collection('Orders').snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text('Something went wrong');
+    final user = FirebaseAuth.instance.currentUser;
+
+    return FutureBuilder<QuerySnapshot>(
+      future: getPastOrders(user!.uid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
@@ -18,35 +24,171 @@ class OrderWidget1 extends StatelessWidget {
             child: CircularProgressIndicator(color: Colors.cyan),
           );
         }
-
-        return GridView.builder(
-            shrinkWrap: true,
-            itemCount: snapshot.data!.size,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4, mainAxisSpacing: 12, crossAxisSpacing: 12),
+        if (snapshot.hasData) {
+          var orders = snapshot.data!.docs;
+          print(orders);
+          return ListView.builder(
+            itemCount: orders.length,
             itemBuilder: (context, index) {
-              final productsData = snapshot.data!.docs[index].get('items');
-              return Column(
-                children: [
-                  Container(
-                    height: 200,
-                    width: 150,
-                    child: Image.network(productsData['productImage']),
+              final orderData = orders[index].data() as Map<String, dynamic>;
+              print('xxxxxxxxxxxxxxx${orderData['items'].length}');
+
+              return Card(
+                elevation: 2.0,
+                //shadowColor:Color(0xFFFF6900),
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Color(0xFFFF6900)),
+                                borderRadius: BorderRadius.circular(20),
+                                color: Color(0xFFFF6900)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                '${orderData['status']}',
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 100.0,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: orderData['items'].length,
+                          itemBuilder: (context, index) {
+                            final product = orderData['items'][index];
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 80.0,
+                                    height: 80.0,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey[200]!,
+                                          blurRadius: 2.0,
+                                          offset: Offset(0.0, 2.0),
+                                        ),
+                                      ],
+                                    ),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      child: Image.network(
+                                        product['productImage'] ??
+                                            'Product image not available',
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 8.0),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        product['productName'] ??
+                                            'Product name not available',
+                                        style: TextStyle(
+                                          fontSize: 12.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                      SizedBox(height: 4.0),
+                                      Text(
+                                        '\$${product['price'].toString()} ',
+                                        style: TextStyle(
+                                          fontSize: 12.0,
+                                        ),
+                                      ),
+                                      SizedBox(height: 4.0),
+                                      Text(
+                                        'Quantity: ${product['quantity'].toString()} ',
+                                        style: TextStyle(
+                                          fontSize: 12.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 16.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Order Date:',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Total:',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '\$${orderData['total'].toString()}',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  Text('Name:  ' + productsData['productName'],
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 15,
-                      )),
-                  Text('Price:  ' + productsData['price'].toString() + ' EGP',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 15,
-                          color: Colors.green)),
-                ],
+                ),
               );
-            });
+            },
+          );
+        } else {
+          return Center(
+            child: Text('No past orders found'),
+          );
+        }
       },
     );
+  }
+
+  Future<QuerySnapshot<Map<String, dynamic>>> getPastOrders(
+      String userId) async {
+    final ordersCollection = FirebaseFirestore.instance.collection('Orders');
+    final pastOrdersQuery = ordersCollection
+        .where('userId', isEqualTo: userId)
+        .orderBy('timestamp', descending: true)
+        .limit(20);
+    final pastOrders = await pastOrdersQuery.get();
+    return pastOrders;
   }
 }
